c3a8e5d13ecd95ff081090e5de1e35f5c1f567fa
nt-arsa <arsa@nashatech.com>
2023-01-11 15:37:37 +0800

    fix script 4

--- db/generateDb.js
+++ db/generateDb.js
@@ -1,9 +1,11 @@
-const fs = require('fs')
-const getContentfulData = require('./contentful')
+const fs = require("fs");
+const getContentfulData = require("./contentful");
 
-;(async () => {
-  const contentfulData = await getContentfulData()
-  const json = JSON.stringify(contentfulData)
-
-  fs.writeFile('./static/contentful.json', json, err => console.log(err))
-})()
+(async () => {
+  const contentfulData = await getContentfulData();
+  const json = JSON.stringify(contentfulData);
+  console.log("HIIIIIIIIIIIIIIIIIIIIIIIIIII");
+  console.log(contentfulData);
+  fs.writeFile("./static/contentful.json", json, (err) => console.log(err));
+  console.log("writed");
+})();
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>Hi</div>
+  <div>
+    Hi
+    {{ contentful }}
+  </div>
 </template>
+
+<script>
+import json from "@/static/contentful.json";
+export default {
+  computed: {
+    contentful() {
+      return json;
+    },
+  },
+  mounted() {
+    console.log(json);
+  },
+};
+</script>
